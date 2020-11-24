8073aa8ee24f40062c7844b2ab560f2f67c2ec9b
Elijah Tristan Huey Chan <Elijah.Tristan.Huey.Chan@asia.ing.com>
2020-11-24 12:10:53 +0800

    Initial commit

---
+++ RewardsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RewardsApp
+//
+//  Created by Elijah Tristan Huey Chan on 11/24/20.
+//  Copyright © 2020 Elijah Tristan Huey Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
